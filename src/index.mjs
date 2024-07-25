f8610f15a245193aeed6c1f00aef568ed18a314d
Bruno Costa <b.costamartins@gmail.com>
2024-07-25 19:58:19 -0300

    Create query params

--- src/index.mjs
+++ src/index.mjs
@@ -6,6 +6,10 @@ const mockUsers = [
   { id: 1, username: "bruno", displayName: "Bruno" },
   { id: 2, username: "caetano", displayName: "Caetano" },
   { id: 3, username: "dandara", displayName: "Dandara" },
+  { id: 4, username: "daniele", displayName: "Daniele" },
+  { id: 5, username: "rodrigo", displayName: "Rodrigo" },
+  { id: 6, username: "maria", displayName: "Maria" },
+  { id: 7, username: "joão", displayName: "João" },
 ];
 
 app.get("/", (req, res) => {
@@ -13,7 +17,15 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/users", (req, res) => {
-  res.send(mockUsers);
+  console.log(req.query)
+  const {
+    query: { filter, value }
+  } = req;
+  //se o filter e o value estiverem vazios
+  if(!filter && !value) return res.send(mockUsers);
+  if(filter && value) return res.send(
+    mockUsers.filter((user) => user[filter].includes(value))
+  )
 });
 
 app.get("/api/users/:id", (req, res) => {
