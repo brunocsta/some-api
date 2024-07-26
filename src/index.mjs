b88ca00875eb9f56cd1a577c0e1366db7a3c5755
Bruno Costa <b.costamartins@gmail.com>
2024-07-26 17:25:03 -0300

    Create the PATCH params

--- src/index.mjs
+++ src/index.mjs
@@ -64,6 +64,19 @@ app.put("/api/users/:id", (req, res) => {
   return res.sendStatus(200)
 });
 
+app.patch("/api/users/:id", (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+  const parsedID = parseInt(id);
+  if (isNaN(parsedID)) return res.sendStatus(400);
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedID);
+  if(findUserIndex === -1) return res.sendStatus(404);
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
+  return res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
   console.log(`Runing on port ${PORT}`);
 });
