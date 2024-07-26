332ddd97fadf195dc9e3439fcb1d881261db7307
Bruno Costa <b.costamartins@gmail.com>
2024-07-26 19:59:26 -0300

    Create the DELETE params

--- src/index.mjs
+++ src/index.mjs
@@ -77,6 +77,19 @@ app.patch("/api/users/:id", (req, res) => {
   return res.sendStatus(200);
 });
 
+
+app.delete("/api/users/:id", (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  const parsedID = parseInt(id);
+  if(isNaN(parsedID)) return res.sendStatus(400);
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedID);
+  if(findUserIndex === -1) return res.sendStatus(404);
+  mockUsers.splice(findUserIndex, 1);
+  return res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
   console.log(`Runing on port ${PORT}`);
 });
